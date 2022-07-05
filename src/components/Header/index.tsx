439fd34944337955ef4e6dc40e83799102cfdd52
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-07-05 20:51:25 -0300

    Finish Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import Logo from '../../../public/logo/logo.svg'
+import Logo from '/logo/logo.svg'
 import styles from './styles.module.scss'
 
 function Header() {
   return (
     <>
     <div className={styles.headerContent}>
-    <img src={Logo} alt="" />
+    <img src={Logo} alt="Logo ToDo" />
     </div>
     </>
   )
