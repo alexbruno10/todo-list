80ad83caf1e34398192541148c593cf1151429d3
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-07-05 21:35:04 -0300

    Setting Inserts

--- src/components/Inserts/index.tsx
+++ src/components/Inserts/index.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import styles from './styles.module.scss'
 import Add from '/logo/add.svg'
 
 function Inserts() {
+
+  const [task, setTask] = useState('');
+
+  function addTask() {
+    console.log(task)
+  }
+
   return (
     <>
     <div className={styles.divContent}>
-        <input placeholder='Adicione uma nova tarefa' type="text" name="task" className={styles.inputContent}/>
-        
+        <input placeholder='Adicione uma nova tarefa'
+        type="text"
+        name="task" 
+        className={styles.inputContent}
+        value={task}
+        onChange={({ target }) => setTask(target.value)}
+        />
         <button
         className={styles.buttonContent}
         type="button"
-        // onClick={onRequestClose}
+        onClick={addTask}
       >
         <img src={Add} alt="Fechal modal" />
       </button>
