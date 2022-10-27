d132d0f537e0b9f0c49a6207c74a726943e064bb
Alex Bruno Neri Humberto <alex.humberto@sicoobcredivar.com.br>
2022-10-27 16:22:49 -0300

    Setting style in task list

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,4 +1,6 @@
 import {Task} from '../Task'
+import './style.scss'
+import Book from '/book.svg'
 
 interface Props {
     task: Task[];
@@ -7,6 +9,32 @@ interface Props {
 export default function Tasks({task}: Props) {
     return (
         <>
+        <div className="tasks">
+            <div className="informationTask">
+                <div className="createdTasks">
+                    <p>
+                        Tarefas criadas
+                    </p>
+                    <span>
+                        10
+                    </span>
+                </div>
+                <div className="completedTasks">
+                    <p>
+                        Concluídas
+                    </p>
+                    <span>
+                        0
+                    </span>
+                </div>
+            </div>
+            <hr/>
+            <div className="tasksNotRegistered">
+                <img src={Book} alt="Image of a book, symbolizing attribution" />
+                <p>Você ainda não tem tarefas cadastradas</p>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+        </div>
         {task.map(tasks => (
             <p key={tasks.id}>{tasks.description}</p>
         ))}
